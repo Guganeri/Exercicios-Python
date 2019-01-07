6e0f358133c5d6df287b0c37d66fe669ea74da0f
Gustavo Neri <35239146+Guganeri@users.noreply.github.com>
2019-01-07 15:08:45 -0200

    Delete #46 - teste.py

--- #46 - teste.py
+++
@@ -1,9 +0,0 @@
-import emoji
-from time import sleep
-
-
-for c in range(10, 0, -1):
-	print(c)
-	sleep(1)
-print(emoji.emojize(':boom::boom::boom::boom::boom::boom::boom::boom::boom::boom::boom::boom::boom::boom::boom::boom:',
-                    use_aliases=True))
