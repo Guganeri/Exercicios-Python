57d9c2d5362e3919ef55ef3bd526555708faec0c
Gustavo Neri Sousa Silva <gustavo.silva.ext@conductor.com.br>
2019-02-12 14:54:43 -0300

    #69

--- #69 - HouM.py
+++ #69 - HouM.py
@@ -1,8 +1,22 @@
-s = ''
-idade = 0
-cont = 1
-while cont = 0:
-    input(s('Digite o seu sexo: '))
-    input(idade('Digite a sua idade: '))
-    input(cont('Deseja continuar?[0]não [1]sim '))
+MaiorIdade = 0
+Homens = 0
+MulherMenor = 0
+continuar = 'SIM'
 
+while continuar in 'SIM':
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo: [M/F] '))
+    if sexo == 'm':
+        Homens += 1
+    if idade >= 18:
+        MaiorIdade += 1
+    if sexo == 'f' and idade < 20:
+        MulherMenor += 1
+    print('-'*20)
+    continuar = str(input('Deseja continua? [S/N]')).upper()
+    print('-'*20)
+
+print('__ FIM __')
+print(f'Maior idade geral:{MaiorIdade}')
+print(f'Quantidade de homens cadastrados:{Homens}')
+print(f'Mulhres menores de 20 anos:{MulherMenor}')
