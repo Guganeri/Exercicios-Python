745fe3d364f830993f920d662647daff8388a0af
Guga Neri <guganeriss@gmail.com>
2019-03-06 13:41:08 -0300

    Teste Lambda

---
+++ Udemy/T.Lambda.py
@@ -0,0 +1,11 @@
+#Testando Lambda
+
+#Verificação se é ou não par retornando Boolean
+par = lambda x: x % 2 == 0
+print(par(3))
+#Primeiro Char
+primeirochar = lambda s:s[0]
+print(primeirochar('Olá Mundo'))
+#Inverter String
+inverter_string = lambda s:s[::-1]
+print(inverter_string('Olá Mundo'))
