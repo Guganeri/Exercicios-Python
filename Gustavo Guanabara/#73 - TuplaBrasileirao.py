23170f04fc6db507c27f6157c094746cd8e50fd3
Gustavo Neri <guganeriss@gmail.com>
2019-03-07 11:28:50 -0300

    #73

---
+++ Gustavo Guanabara/#73 - TuplaBrasileirao.py
@@ -0,0 +1,13 @@
+campeonato = ('Palmeiras','Flamengo','Athletico-PR',
+              'Atlético-MG', 'Avaí', 'Bahia', 'Botafogo', 'CSA', 'Ceará',
+              'Chapecoense', 'Corinthians','São Paulo', 'Chapecoense', 'Fluminense', 'Santos', 'Internacional')
+
+print('='*15,'Os 5 primeiros colocados são:','='*15)
+print(campeonato[0:5])
+print('='*15,'Os 4 últimos colocados são:','='*15)
+print(campeonato[-4:])
+print('='*50)
+print('Times em ordem alfab:')
+print(sorted(campeonato))
+print(f'O chapeconse esta na {campeonato.index("Chapecoense")+1}º posição')
+
