2d20eb8d5cc78a6638462fa6f9db3cf6b313c16c
Guga Neri <guganeriss@gmail.com>
2019-03-12 22:45:59 -0300

    08

---
+++ Udemy/AV.08.py
@@ -0,0 +1,10 @@
+def ran_check(num,low,high):
+    if num in list(range(low,high+1)):
+        print(num)
+        print(low)
+        print(high)
+        return True
+    else:
+        return False
+
+print(ran_check(3,1,10)
