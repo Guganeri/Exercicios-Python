754f46e1bd38cbf3708fc70cc3bc2ec257e089b3
Gustavo Neri <guganeriss@gmail.com>
2019-03-18 09:34:59 -0300

    add

--- Jogo da Velha/JogoDaVelha.py
+++ Jogo da Velha/JogoDaVelha.py
@@ -1,1 +1,5 @@
 #Desafio Udemy criação do Jogo da Velha em Python
+from IPython.display import clear_output
+
+player1 = str(input('Digite o nome do jogador um:'))
+player2 = str(input('Digite o nome do Jogador dois:'))
