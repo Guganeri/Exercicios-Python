32486f5988f89bf154c63cec63bd9267773b9b0c
Gustavo Neri Sousa Silva <gustavo.silva.ext@conductor.com.br>
2019-03-18 17:20:46 -0300

    add

--- Jogo da Velha/JogoDaVelha.py
+++ Jogo da Velha/JogoDaVelha.py
@@ -1,5 +1,33 @@
 #Desafio Udemy criação do Jogo da Velha em Python
 from IPython.display import clear_output
 
-player1 = str(input('Digite o nome do jogador um:'))
-player2 = str(input('Digite o nome do Jogador dois:'))
+def display_board(board):
+    clear_output()
+    print('   |   |   ')
+    print(' '+board[7]+' | '+board[8]+' | '+board[9])
+    print('   |   |   ')
+    print('-------------')
+    print('   |   |   ')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |   ')
+    print('-------------')
+    print('   |   |   ')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |   ')
+    pass
+def player_input():
+    input()
+    market = ''
+    while not (market == 'X' or market == 'O'):
+        market = input('Player1: Você quer ser X ou O?').upper()
+    if market == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
+def placa_marker(board, marker, position):
+    board[position] = marker
+
+
+
+display_board([" ", " "," ", " ", " "," ", " "," ", " ", " "])
