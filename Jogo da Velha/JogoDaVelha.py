42eb07fac356b1400084b8536af8d94e8c697ef7
Guga Neri <guganeriss@gmail.com>
2019-03-18 22:22:51 -0300

    add

--- Jogo da Velha/JogoDaVelha.py
+++ Jogo da Velha/JogoDaVelha.py
@@ -48,4 +48,21 @@ def chose_first():
         return 'Player 1'
     pass
 
+def space_check(board,position):
+
+    return board[position] == ' '
+
+def full_board_check(board):
+    for i in range (0,10):
+        if space_check(board, i):
+            return False
+
+    return True
+
+def player_choice(board):
+    position = ' '
+    while position not in '1, 2, 3, 4, 5, 6, 7, 8, ,9'.split():
+        position = input('Escolha sua jogada (1-9) ')
+
+
 display_board([" ", " "," ", " ", " "," ", " "," ", " ", " "])
