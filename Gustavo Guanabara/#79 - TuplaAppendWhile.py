866d76ccf4f6cedd83b44d934137fc9c77741f11
Gustavo Neri Sousa Silva <gustavo.silva.ext@conductor.com.br>
2019-03-20 16:58:45 -0300

    add

--- Gustavo Guanabara/#79 - TuplaAppendWhile.py
+++ Gustavo Guanabara/#79 - TuplaAppendWhile.py
@@ -2,21 +2,19 @@ list = []
 
 while True:
     numero = int(input('Digite um numero: '))
-    list.append(numero)
+    if numero not in list:
+        list.append(numero)
+        print('Adcionado com sucesso')
+    else:
+        print('Valor duplicado não adcionado')
     str = input('Deseja continuar? [S/N] ').upper()
-    print('Adcionado com sucesso ...')
     if str == 'S':
         continue
     else:
         print('-='*25)
         break
-
 print(sorted(set(list)))
-#if n not in numero?
-#    numero.append(n)
-#    print('Valor Adcional')
-#else:
-#   print('Valor duplicado não adcionar...')
+
 
 
 
