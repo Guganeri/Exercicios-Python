e799385bf6671438b2a469a79bc563cae6118095
Gustavo Neri Sousa Silva <gustavo.silva.ext@conductor.com.br>
2019-03-20 15:52:49 -0300

    add

--- Gustavo Guanabara/#79 - TuplaAppendWhile.py
+++ Gustavo Guanabara/#79 - TuplaAppendWhile.py
@@ -1,12 +1,13 @@
 list = []
 
 while True:
-    numero = int(input('Digite um numero'))
+    numero = int(input('Digite um numero: '))
     list.append(numero)
     str = input('Deseja continuar? S/N').upper()
     if str == 'S':
         continue
     else:
+        print('='*25)
         break
 
 print(list)
