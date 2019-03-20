909b91e3aaf51784f38cdfa539ab48f029726735
Gustavo Neri <guganeriss@gmail.com>
2019-03-20 11:14:23 -0300

    add

--- Gustavo Guanabara/#78 - ListBigSmall.py
+++ Gustavo Guanabara/#78 - ListBigSmall.py
@@ -1,7 +1,25 @@
 valores = []
-print('Digite 5 valores')
-for numero in range(0, 5):
-    valores.append(int(input('Digite um valor: ')))
+max = 0
+min = 0
 
+for contador in range(0, 5):
+    valores.append(int(input('Digite um valor: ')))
+    if contador == 0:
+        max = min = valores[contador]
+    else:
+        if valores[contador] > max:
+            max = valores[contador]
+        if valores[contador] < min:
+            min = valores[contador]
+
+print(min)
+print(max)
+print('Lista')
 print(valores)
-print(valores.sort())
+
+for indice, valor in enumerate(valores):
+    if valor == max:
+        print(f'A posição do maior valor no indice é:{indice}')
+    if valor == min:
+        print(f'A posição do menor valor no indice é:{indice}')
+
