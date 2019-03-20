972c400b2d34b2adad9ed69978e0cd5f8f3177a2
Gustavo Neri Sousa Silva <gustavo.silva.ext@conductor.com.br>
2019-03-20 18:16:55 -0300

    add

--- Gustavo Guanabara/#80 - GuardarValoresOrdenadamente.py
+++ Gustavo Guanabara/#80 - GuardarValoresOrdenadamente.py
@@ -1,5 +1,15 @@
 lista = []
 
-for n in range(0,5):
-    n = input(input('Digite um numero: '))
-    
+for c in range(0, 5):
+    n = int(input('Digite um numero: '))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                break
+            pos += 1
+
+print(lista)
