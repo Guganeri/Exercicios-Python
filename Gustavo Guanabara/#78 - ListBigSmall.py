1d990b8619ad91665db24dfeee71e65f1aed9700
Gustavo Neri <guganeriss@gmail.com>
2019-03-20 10:17:48 -0300

    add

---
+++ Gustavo Guanabara/#78 - ListBigSmall.py
@@ -0,0 +1,7 @@
+valores = []
+print('Digite 5 valores')
+for numero in range(0, 5):
+    valores.append(int(input('Digite um valor: ')))
+
+print(valores)
+print(valores.sort())
---
+++ Gustavo Guanabara/Example - Lista.py
@@ -0,0 +1,8 @@
+valores = []
+valores.append(5)
+valores.append(9)
+valores.append(2)
+valores.append(1)
+
+for valor in valores:
+    print(f'{valor}...', end='')
