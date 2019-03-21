e49af2374c3219867ace1337e7334f29d0575faa
Gustavo Neri <guganeriss@gmail.com>
2019-03-21 11:03:42 -0300

    add 81

--- Gustavo Guanabara/#81 - ListConsultOrderbyLen.py
+++ Gustavo Guanabara/#81 - ListConsultOrderbyLen.py
@@ -1,9 +1,8 @@
 numeros = list()
-
+count = 0
 
 while True:
     n = int(input('Digite um numero: '))
-    count = 0
     if count == 0:
         numeros.append(n)
     else:
@@ -11,19 +10,16 @@ while True:
         while pos < len(numeros):
             if n <= numeros[pos]:
                 numeros.insert(numeros[pos])
-            pos += 1
-
-
-
-    count += 1
+                pos += 1
 
     choice = str(input('Deseja continuar?[S/N]')).upper()
     if choice == 'S':
         print('Guardando valores...')
     else:
         break
+    count += 1
 
 print(f'O tamanho da lista é: {len(numeros)}')
-print(f'Existe o numero 5 na posição {numeros.index+1(5)}')
-print(numeros)
+print(f'Existe o numero 5 na posição {numeros.index(5)}')
+print(count)
 
