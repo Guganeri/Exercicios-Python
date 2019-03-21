07e94dc1b9c7d749152606dfce9952089f658e4c
Gustavo Neri <guganeriss@gmail.com>
2019-03-21 10:31:03 -0300

    add

--- Gustavo Guanabara/#81 - ListConsultOrderbyLen.py
+++ Gustavo Guanabara/#81 - ListConsultOrderbyLen.py
@@ -12,10 +12,5 @@ while True:
 
 print(f'O tamanho da lista é: {len(numeros)}')
 print(f'Os elementos estão ordenados de forma decrescente: {sorted(numeros[::-1])}')
-for indice, valores in enumerate(numeros):
-    if 5 in numeros:
-        print(f'Existe o valor 5 na posição {indice}')
-        break
-    else:
-        continue
-    indice += 1
+print(f'Existe o numero 5 na posição {numeros.index(5)}')
+
