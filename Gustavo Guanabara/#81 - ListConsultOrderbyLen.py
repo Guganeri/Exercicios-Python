c5b158764b21e0f0652f3a1ffd0700de73340ac6
Gustavo Neri <guganeriss@gmail.com>
2019-03-21 10:38:05 -0300

    add

--- Gustavo Guanabara/#81 - ListConsultOrderbyLen.py
+++ Gustavo Guanabara/#81 - ListConsultOrderbyLen.py
@@ -11,6 +11,7 @@ while True:
         break
 
 print(f'O tamanho da lista é: {len(numeros)}')
-print(f'Os elementos estão ordenados de forma decrescente: {sorted(numeros[::-1])}')
+for x in numeros[::-1]:
+    print(x, end=' ')
 print(f'Existe o numero 5 na posição {numeros.index(5)}')
 
