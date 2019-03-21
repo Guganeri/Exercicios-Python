5cb92636889bd8542cb4e96af40ffdfe8a5fab32
Gustavo Neri <guganeriss@gmail.com>
2019-03-21 08:43:55 -0300

    ad

--- Gustavo Guanabara/#81 - ListConsultOrderbyLen.py
+++ Gustavo Guanabara/#81 - ListConsultOrderbyLen.py
@@ -3,5 +3,7 @@ numeros = list()
 while True:
     n = int(input('Digite um numero: '))
     str = str(input('Deseja Continuar? [S/N] ')).upper
-    if str != 'S':
+    if str == 'S':
+        continue
+    else:
         break
