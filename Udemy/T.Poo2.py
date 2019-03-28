b9f3796282fe9b59bccbbdf62a94c91e77e2cdac
Guga Neri <guganeriss@gmail.com>
2019-03-28 16:59:14 -0300

    add

--- Udemy/T.Poo2.py
+++ Udemy/T.Poo2.py
@@ -1,1 +1,16 @@
+class Circulo(object):
+    pi = 3.14
 
+    def _init_(self, raio=1):
+        self.raio = raio
+
+    def area(self):
+        return self.raio **2 * self.pi
+
+    def defRaio(self, raio):
+        self.raio = raio
+
+    def obtemRaio(self):
+        return self.raio
+
+c = Circulo
