d83701fadafd5edd8664d389bed5cf02c0127a7d
GUGA NERI <guganeriss@gmail.com>
2019-04-15 10:31:04 -0300

    add inicial crud

---
+++ Udemy/T.POOCRUD.py
@@ -0,0 +1,16 @@
+class Book(object):
+    def __init__(self, titulo, autor, paginas):
+        print('Um livro foi criado')
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+
+    def __str__(self):
+        return "Titulo (a)".format(a=self.titulo)
+    def __len__(self):
+        return self.paginas
+    def __del__(self):
+        print("Livro Destruido")
+
+livro1 = Book('Pyhton', 'Gustavo',100)
+len(livro1)
