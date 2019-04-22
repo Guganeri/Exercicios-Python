b8ddff64aaf4af79d897d6345eef90c4a1f90f4a
GUGA NERI <guganeriss@gmail.com>
2019-04-22 10:18:46 -0300

    add

--- Udemy/Av.14.py
+++ Udemy/Av.14.py
@@ -1,4 +1,8 @@
-class Line(object):
+import print as print
+
+
+class Line():
+
     def __init__(self,coor1,coor2):
         self.coor1 = coor1
         self.coor2 = coor2
@@ -12,7 +16,8 @@ class Line(object):
     def slope(self):
         x1,y1 = self.coor1
         x2,y2 = self.coor2
-        return ((y2-y1))/(x2-x2)
+        return float((y2-y1))/(x2-x2)
 
-l1 = line()
-l1.distance(3,2)
+l1 = Line(3,2)
+print(l1.distance(3,2))
+
