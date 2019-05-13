f18e9704faa91e34b382c918f7fb80cd380dd430
Guga Neri <guganeriss@gmail.com>
2019-05-12 23:28:39 -0300

    alteração 84

--- Gustavo Guanabara/#84 - Lista.py
+++ Gustavo Guanabara/#84 - Lista.py
@@ -1,9 +1,16 @@
 temp = []
 princ = []
-
+mai = men = 0
 while True:
     temp.append(str(input('Nome: ')))
     temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
     princ.append(temp[:])
     temp.clear()
 
@@ -11,3 +18,6 @@ while True:
     if resp in 'Nn':
         break
 print(f'Os dados foram {princ}')
+print(f'Ao todo você cadastrou {len(princ)} pessoas')
+print(f'O maior peso foi {mai}kg')
+print(f'O menor peso foi {men}kg')
