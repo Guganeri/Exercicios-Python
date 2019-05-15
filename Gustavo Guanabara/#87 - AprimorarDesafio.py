b192a729e74b66ef91695693c03616f1e0388963
Guga Neri <guganeriss@gmail.com>
2019-05-15 11:58:06 -0300

    #84

--- Gustavo Guanabara/#87 - AprimorarDesafio.py
+++ Gustavo Guanabara/#87 - AprimorarDesafio.py
@@ -1,6 +1,6 @@
 ### Resolução Guanabara ###
 matriz2 = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-l2 = somatot = 0
+maiorsegundalinha = somatot = 0
 somaterceiracoluna = 0
 
 for linha in range(0, 3):
@@ -19,14 +19,14 @@ for linha in range(0, 3):
 
 somaterceiracoluna += matriz2[0][2] + matriz2[1][2] + matriz2[2][2]
 
-if l2 == 0:
-    l2 = matriz2[2][0]
-    if l2 > matriz2[2][1]:
-
-    l2 += matriz2[2][2]
-
+if maiorsegundalinha == 0:
+    maiorsegundalinha = matriz2[1][0]
+    if maiorsegundalinha < matriz2[1][1]:
+        maiorsegundalinha = matriz2[1][1]
+    if maiorsegundalinha <matriz2[1][2]:
+        maiorsegundalinha = matriz2[1][2]
 
 print('-=' * 30)
 print(f'A soma dos numeros pares é: {somatot}')
 print(f'Soma dos valores da terceira coluna é: {somaterceiracoluna}')
-print(f'O maior valor da segunda linha  ')
+print(f'O maior valor da segunda linha {maiorsegundalinha}')
