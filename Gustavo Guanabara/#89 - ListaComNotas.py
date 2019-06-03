deffa31ab08f19e5455d6d2ae0a10f57b700bdb2
Guga Neri <guganeriss@gmail.com>
2019-06-03 08:44:33 -0300

    89

--- Gustavo Guanabara/#89 - ListaComNotas.py
+++ Gustavo Guanabara/#89 - ListaComNotas.py
@@ -1,13 +1,27 @@
-gerallist = list()
-alunolist = list()
+ficha = list()
+
 while True:
+    nome = str(input('Digite o nome do aluno: '))
+    nota1 = float(input('Digite a primeira nota do aluno: '))
+    nota2 = float(input('Digite a segunda nota do aluno: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Deseja continuar? [S/N]'))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'{"No.":<4}{"NOME":<10}{"MEDIA":>8}')
+print('-' * 26)
 
-    nome = str(input('Digite o nome'))
-    alunolist.append(nome)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-=' * 35)
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
 
-    nota = list(input('Digite a primeira nota: '))
-    alunolist.append(nota[0])
-    nota = list(input('Digite a segunda nota: '))
-    alunolist.append(nota[1])
-    gerallist.append(alunolist)
-    break
+    if opc == 999:
+        print('FINALIZANDO... ')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print("<<< VOLTE SEMPRE >>>")
